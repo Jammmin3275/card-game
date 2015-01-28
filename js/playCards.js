3d0cab8b8e9e318b3f4b05091354badc18615b8e
Jammmin3275 <zombaniac64@yahoo.com>
2015-01-27 16:14:11 -0800

    Update playCards.js

--- js/playCards.js
+++ js/playCards.js
@@ -15,6 +15,12 @@ $(document).ready(function(){
         for(var i=0;i<hand.length;i++){
             el.append(hand[i].getHTML());
         }
+         var showHand = function(){
+        var el = $('#cpuHand')
+        el.html('');
+        for(var i=0;i<hand1.length;i++){
+            el.append(hand1[i].getHTML());
+        }
     }
     var doShuffle = function(){
         cardDeck.shuffle();
